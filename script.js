a847e19866cc4b7c06533e21a5fdafab2a9378dd
LhordeS <lhorde@msn.com>
2022-06-11 09:37:45 +0900

    Creat first two js functions. Not retutrning correct value when player wins.

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const rpsArray = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay() {
+    const compResult = Math.floor(Math.random() * rpsArray.length);
+    return rpsArray[compResult];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if ((computerSelection === 'Rock' && playerSelection === 'Scissors') || (computerSelection === 'Paper' && playerSelection === 'Rock') || (computerSelection === 'Scissors' && playerSelection === 'Paper')) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return "It's a tie! Try again"
+    }
+}
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+console.log(playerSelection, computerSelection);
+console.log(playRound('Rock', computerPlay()));
